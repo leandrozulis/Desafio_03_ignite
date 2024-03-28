f6f568c45efab93351d9f754c3955deb7b31a0ea
Leandro Zulli Silva <leandro_zulli@hotmail.com>
2024-03-27 22:31:27 -0300

    Create RefreshToken para o front utilizar quando o token expirar

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,22 @@ import { ZodError } from "zod";
 import { env } from "./env";
 import { routePet } from "./http/controllers/Pet/routePet";
 import fastifyJwt from "@fastify/jwt";
+import fasitfyCookie from "@fastify/cookie"
 
 export const app = fastify()
 
 app.register(fastifyJwt, {
-  secret: env.JWT_SECRET
+  secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false
+  },
+  sign: {
+    expiresIn: '10m'
+  }
 })
+
+app.register(fasitfyCookie)
 
 app.register(routeUser)
 app.register(routePet)
--- src/http/controllers/User/routes.ts
+++ src/http/controllers/User/routes.ts
@@ -3,10 +3,13 @@ import { register } from "./register";
 import { authenticate } from "./authenticate";
 import { profile } from "./profile";
 import { verifyJWT } from "@/http/middlewares/verify-jwt";
+import { refresh } from "./refreshToken";
 
 export async function routeUser(app: FastifyInstance) {
   app.post('/create', register)
   app.post('/authenticate', authenticate)
+
+  app.patch('/token/refresh', refresh)
 
   app.get('/me', { onRequest: [verifyJWT] }, profile)
 }
